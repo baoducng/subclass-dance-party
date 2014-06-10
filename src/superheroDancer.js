231c00a0fceaf80305cdb9837ccaccbe194ead31
Hack Reactor Students <accounts@hackreactor.com>
2014-06-10 12:23:25 -0700

    Refactored superhero subclasses

--- src/superheroDancer.js
+++ src/superheroDancer.js
@@ -1,14 +1,33 @@
 var MakeSuperheroDancer = function(top, left, timeBetweenSteps){
   MakeDancer.apply(this, arguments);
   this.$node.addClass('superhero');
+  this._bounceLength = Math.random() * 200;
+  this._bounceTime = Math.random() * 5 + 100;
 };
 
 MakeSuperheroDancer.prototype = Object.create(MakeDancer.prototype);
 MakeSuperheroDancer.prototype.constructor = MakeSuperheroDancer;
 
-MakeSuperheroDancer.prototype.step = function(){
-  MakeDancer.prototype.step.call(this);
-  this.$node.toggle();
+MakeSuperheroDancer.prototype.step = function(bounceType){
+  var context = this;
+  var outerArgs;
+  var innerArgs;
+
+  if (bounceType === 'top') {
+    outerArgs = {'top': context._top - context._bounceLength};
+    innerArgs = {'top': context._top + context._bounceLength};
+  }
+
+  if (bounceType === 'left') {
+    outerArgs = {'left': context._left - context._bounceLength};
+    innerArgs = {'left': context._left + context._bounceLength};
+  }
+
+  this.$node.animate(outerArgs, context._bounceTime, 'linear', function() {
+    context.$node.animate(innerArgs, context._bounceTime, 'linear', function(){
+      setInterval(context.step.bind(context), 1000);
+    });
+  });
 };
 
 
@@ -22,18 +41,8 @@ Ironman.prototype = Object.create(MakeSuperheroDancer.prototype);
 Ironman.prototype.constructor = Ironman;
 
 Ironman.prototype.step = function(){
-  var bounceTime = 200;
-  var bounceLength = 100;
-  var context = this;
-
-  this.$node.animate({'top': this._top - bounceLength}, bounceTime, 'linear', function() {
-    context.$node.animate({'top': context._top + bounceLength}, bounceTime, 'linear', function(){
-      setInterval(context.step.bind(context), 1000);
-    });
-  });
-
+  MakeSuperheroDancer.prototype.step.call(this, 'top');
 };
-
 
 
 var Batman = function(top, left, timeBetweenSteps){
@@ -46,16 +55,7 @@ Batman.prototype = Object.create(MakeSuperheroDancer.prototype);
 Batman.prototype.constructor = Batman;
 
 Batman.prototype.step = function(){
-  var bounceTime = 200;
-  var bounceLength = 100;
-  var context = this;
-
-  this.$node.animate({'left': this._left - bounceLength}, bounceTime, 'linear', function() {
-    context.$node.animate({'left': context._left + bounceLength}, bounceTime, 'linear', function(){
-      setInterval(context.step.bind(context), 1000);
-    });
-  });
-
+  MakeSuperheroDancer.prototype.step.call(this, 'left');
 };
 
 
