e63352c91e8c2d82291c84c342ce73dbd9e471bb
Hack Reactor Students <accounts@hackreactor.com>
2014-06-09 19:40:03 -0700

    added background, dancer images, google web fonts, some styling

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -33,3 +33,75 @@ describe("blinkyDancer", function() {
     });
   });
 });
+
+describe("popDancer", function(){
+
+  var popDancer;
+  var timeBetweenSteps = 100;
+  var clock;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    popDancer = new MakePopDancer(10, 20, timeBetweenSteps);
+  });
+
+  it("should have a jQuery $node object", function(){
+    expect(popDancer.$node).to.be.an.instanceof(jQuery);
+  });
+
+  it("should have a step function that makes its node blink", function() {
+    sinon.spy(popDancer.$node, 'toggle');
+    popDancer.step();
+    expect(popDancer.$node.toggle.called).to.be.true;
+  });
+
+  describe("dance", function(){
+    it("should call step at least once per second", function(){
+      sinon.spy(popDancer, "step");
+      expect(popDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+
+      expect(popDancer.step.callCount).to.be.equal(1);
+
+      clock.tick(timeBetweenSteps);
+      expect(popDancer.step.callCount).to.be.equal(2);
+    });
+  });
+});
+
+describe("superheroDancer", function(){
+
+  var superheroDancer;
+  var timeBetweenSteps = 100;
+  var clock;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    superheroDancer = new MakeSuperheroDancer(10, 20, timeBetweenSteps);
+  });
+
+  it("should have a jQuery $node object", function(){
+    expect(superheroDancer.$node).to.be.an.instanceof(jQuery);
+  });
+
+  it("should have a step function that makes its node blink", function() {
+    sinon.spy(superheroDancer.$node, 'toggle');
+    superheroDancer.step();
+    expect(superheroDancer.$node.toggle.called).to.be.true;
+  });
+
+  describe("dance", function(){
+    it("should call step at least once per second", function(){
+      sinon.spy(superheroDancer, "step");
+      expect(superheroDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+
+      expect(superheroDancer.step.callCount).to.be.equal(1);
+
+      clock.tick(timeBetweenSteps);
+      expect(superheroDancer.step.callCount).to.be.equal(2);
+    });
+  });
+});
